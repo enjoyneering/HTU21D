f73a45ac56ec7886db786ae8664c7e54f7ddf451
enjoyneering <enjoyneering79@gmail.com>
2018-02-01 16:07:02 -0500

    Update HTU21D_ESP8266_Humidex_Windchill.ino

--- examples/HTU21D_ESP8266_Humidex_Windchill/HTU21D_ESP8266_Humidex_Windchill.ino
+++ examples/HTU21D_ESP8266_Humidex_Windchill/HTU21D_ESP8266_Humidex_Windchill.ino
@@ -6,42 +6,33 @@
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
 
-  This sensor uses I2C bus to communicate. Two pins are required to interface
+  This sensor uses I2C bus to communicate, specials pins are required to interface
 
-  Connect sensor to pins:  SDA     SCL
-  Uno, Mini, Pro:          A4      A5
-  Mega2560, Due:           20      21
-  Leonardo:                2       3
-  ATtiny85:                0(5)    2/A1(7) (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                            & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-xx:          ANY     ANY     (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             ANY     ANY     (D2 & D1 by default)
+  Connect chip to pins:    SDA        SCL
+  Uno, Mini, Pro:          A4         A5
+  Mega2560, Due:           20         21
+  Leonardo:                2          3
+  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
+                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
+  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
+  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
+  WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
 
-  NOTE:
-  The Humidex is an index number used to describe how hot the weather feels 
-  to the average person, by combining the effect of heat and humidity.
-  Humidex is calculated as:
-                            Humidex = air temperature + h
-                            h = (0.5555) * (e - 10.0)
-                            e = 6.11 * exp(5417.7530 * ((1 / 273.16) - (1 / dewpoint)))
-
-  Wind chill is an index number used to describe how cold the weather feels
-  to the average person, by combining the effect of cold and wind speeds.
-  Wind chill is calculated:
-                            Windchill = 13.12 + 0.6215 * ta - 11.37 * v^0.16 + 0.3965 * ta * v^0.16
-                            ta = air temperature below 10C
-                            v  = wind velocity greater than 4.8 km/h
-
-  BSD license, all text above must be included in any redistribution
+  GNU GPL license, all text above must be included in any redistribution, see link below for details
+  - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
-#include <Wire.h>
+#include <Wire.h>              //https://github.com/enjoyneering/ESP8266-I2C-Driver
 #include <HTU21D.h>
 #include <LiquidCrystal_I2C.h> //https://github.com/enjoyneering/LiquidCrystal_I2C
+#include <ESP8266WiFi.h>
 
 #define LCD_ROWS      4        //qnt. of lcd rows
 #define LCD_COLUMNS   20       //qnt. of lcd columns
-#define DEGREE_SYMBOL 0xDF     //degree symbol from the LCD ROM
+
+#define DEGREE_SYMBOL 0xDF     //degree symbol from the lcd ROM
+#define SPACE_SYMBOL  0x20     //space  symbol from lcd ROM
+
 #define WIND_SPEED    5        //assuming wind speed is 5 m/sec
 #define ERROR         255      //error code
 
@@ -53,13 +44,32 @@ float humidity    = 0;
 float dewpoint    = 0;
 float humidex     = 0;
 float windchill   = 0;
- 
+
+/*
+HTU21D(resolution)
+
+resolution:
+HTU21D_RES_RH12_TEMP14 - RH: 12Bit, Temperature: 14Bit, by default
+HTU21D_RES_RH8_TEMP12  - RH: 8Bit,  Temperature: 12Bit
+HTU21D_RES_RH10_TEMP13 - RH: 10Bit, Temperature: 13Bit
+HTU21D_RES_RH11_TEMP11 - RH: 11Bit, Temperature: 11Bit
+*/ 
 HTU21D            myHTU21D(HTU21D_RES_RH12_TEMP14);
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 
+/**************************************************************************/
+/*
+    setup()
+
+    Main setup
+*/
+/**************************************************************************/
 void setup()
 {
+  WiFi.enableAP(false);                                         //disable soft AP
+  WiFi.enableSTA(false);                                        //disable station/client
+
   Serial.begin(115200);
 
   /* LCD connection check */  
@@ -76,11 +86,13 @@ void setup()
   while (myHTU21D.begin() != true)
   {
     lcd.setCursor(0, 0);
-    lcd.print("HTU21D error ");
+    lcd.print("HTU21D error");
     delay(5000);
   }
-  lcd.print("HTU21D OK   ");
-  delay(2000);
+  lcd.clear();
+
+  lcd.print("HTU21D OK");
+  delay(1000);
   lcd.clear();
 
   /* load custom symbol to CGRAM */
@@ -88,10 +100,10 @@ void setup()
   lcd.createChar(1, humidity_icon);
 
   /* prints static text */
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);                                                  //set 1-st colum & 1-st row, first colum & row started at zero
   lcd.write((uint8_t)0);                                                //print custom tempereture symbol
 
-  lcd.setCursor(10, 0);                                                 //set 11-th colum & 2-nd row. NOTE: first colum & row started at zero
+  lcd.setCursor(10, 0);
   lcd.write((uint8_t)1);                                                //print custom humidity symbol
 
   lcd.setCursor(0, 1);
@@ -104,80 +116,115 @@ void setup()
   lcd.print("Windchill:");
 }
 
+/**************************************************************************/
+/*
+    loop()
+
+    Main loop
+*/
+/**************************************************************************/
 void loop()
 {
   humidity    = myHTU21D.readCompensatedHumidity();
   temperature = myHTU21D.readTemperature();
+
   dewpoint    = calculateDewPoint(temperature, humidity);
   humidex     = calculateHumidex(temperature, dewpoint);
   windchill   = calculateWindchill(temperature, WIND_SPEED);
 
-  /* prints dynamic text & data */
+  /* prints dynamic temperature data */
   lcd.setCursor(1, 0);
-  if (temperature == HTU21D_ERROR)
+  switch (temperature)
   {
-    lcd.print("---");
-  }
-  else
-  { 
-    lcd.print(temperature);
-    lcd.write(DEGREE_SYMBOL);
-  }
+    case HTU21D_ERROR:
+      lcd.print("xx");
+      break;
 
+    default:
+      lcd.print(temperature);
+      break;
+  }
+  lcd.write(DEGREE_SYMBOL);
+  lcd.write(SPACE_SYMBOL);
+
+  /* prints dynamic humidity data */
   lcd.setCursor(11, 0);
-  if (humidity == HTU21D_ERROR)
+  switch (humidity)
   {
-    lcd.print("---");
-  }
-  else
-  { 
-    lcd.print(humidity);
-    lcd.print("% ");
-  }
+    case HTU21D_ERROR:
+      lcd.print("xx");
+      break;
 
+    default:
+      lcd.print(humidity);
+      break;
+  }
+  lcd.print("%");
+  lcd.write(SPACE_SYMBOL);
+
+  /* prints dynamic dewpoint data */
   lcd.setCursor(9, 1);
-  if (dewpoint == ERROR)
+  switch (dewpoint)
   {
-    lcd.print("---");
-  }
-  else
-  {
-    lcd.print(dewpoint);
-    lcd.write(DEGREE_SYMBOL);
-  }
+    case ERROR:
+      lcd.print("xx");
+      break;
 
+    default:
+      lcd.print(dewpoint);
+      break;
+  }
+  lcd.write(DEGREE_SYMBOL);
+  lcd.write(SPACE_SYMBOL);
+
+  /* prints dynamic humidex data */
   lcd.setCursor(9, 2);
-  if (humidex == ERROR)
+  switch (humidex)
   {
-    lcd.print("---");
-  }
-  else
-  {  
-    lcd.print(humidex);
-    lcd.write(DEGREE_SYMBOL);
-  }
+    case ERROR:
+      lcd.print("xx");
+      break;
 
+    default:
+      lcd.print(humidex);
+      break;
+  }
+  lcd.write(DEGREE_SYMBOL);
+  lcd.write(SPACE_SYMBOL);
+
+  /* prints dynamic windchill data */
   lcd.setCursor(9, 3);
-  if (windchill == ERROR)
+  switch (windchill)
   {
-    lcd.print("---");
+    case ERROR:
+      lcd.print("xx");
+      break;
+
+    default:
+      lcd.print(windchill);
+      break;
   }
-  else
-  {
-    lcd.print(windchill);
-    lcd.write(DEGREE_SYMBOL);
-  }
+  lcd.write(DEGREE_SYMBOL);
+  lcd.write(SPACE_SYMBOL);
+
 
   /* prints serial data */
   Serial.print("Humidity............: "); Serial.print(humidity);    Serial.println("%");
   Serial.print("Temperature.........: "); Serial.print(temperature); Serial.println("C");
   Serial.print("Dew Point...........: "); Serial.print(dewpoint);    Serial.println("C");
-  Serial.print("Humidex/Feels like..: "); Serial.print(humidex);     Serial.print("C, ");  HumidexDiscomfortIndex(humidex);
-  Serial.print("Windchill/Feels like: "); Serial.print(windchill);   Serial.print("C, ");  WindchillDiscomfortIndex(humidex);
+  Serial.print("Humidex/Feels like..: "); Serial.print(humidex);     Serial.print("C, "); humidexDiscomfortIndex(humidex);
+  Serial.print("Windchill/Feels like: "); Serial.print(windchill);   Serial.print("C, "); windchillDiscomfortIndex(humidex);
 
   delay(20000);
 }
- 
+
+/**************************************************************************/
+/*
+    calculateDewPoint()
+
+    Dew point calculation
+*/
+/**************************************************************************/
 /* dew point calculation */
 float calculateDewPoint(float temperature, float humidity)
 {
@@ -194,12 +241,26 @@ float calculateDewPoint(float temperature, float humidity)
   return ERROR;
 }
 
-/* humidex calculation */
+/**************************************************************************/
+/*
+    calculateHumidex()
+
+    Humidex calculation
+
+    Humidex is an index number used to describe how hot the weather feels 
+    to the average person, by combining the effect of heat and humidity.
+
+    Humidex is calculated as:
+       Humidex = air temperature + h
+       h = (0.5555) * (e - 10.0)
+       e = 6.11 * exp(5417.7530 * ((1 / 273.16) - (1 / dewpoint)))
+*/
+/**************************************************************************/
 float calculateHumidex(float temperature, float dewpoint)
 {
   if (temperature > 5 && temperature != HTU21D_ERROR && dewpoint != ERROR)
   {
-    float e = 5417.7530 * ((1 / 273.16) - (1 / (273.16 + dewpoint)));
+    float e       = 5417.7530 * ((1 / 273.16) - (1 / (273.16 + dewpoint)));
     float humidex = temperature + 0.5555 * ( 6.11 * exp(e) - 10);
 
     return (humidex);
@@ -207,7 +268,21 @@ float calculateHumidex(float temperature, float dewpoint)
   return ERROR;
 }
 
-/* windchill calculation */
+/**************************************************************************/
+/*
+    calculateWindchill()
+
+    Windchill calculation
+
+    Wind chill is an index number used to describe how cold the weather feels
+    to the average person, by combining the effect of cold and wind speeds.
+
+    Wind chill is calculated:
+       Windchill = 13.12 + 0.6215 * ta - 11.37 * v^0.16 + 0.3965 * ta * v^0.16
+       ta = air temperature below 10C
+       v  = wind velocity greater than 4.8 km/h
+*/
+/**************************************************************************/
 float calculateWindchill(float temperature, float velocity)
 {
   if (temperature != HTU21D_ERROR && temperature <= 5 && velocity >= 5)
@@ -219,8 +294,14 @@ float calculateWindchill(float temperature, float velocity)
   return ERROR;
 }
 
-/* humidex discomfort levels */
-void HumidexDiscomfortIndex(float humidex)
+/**************************************************************************/
+/*
+    humidexDiscomfortIndex()
+
+    Describes levels of humidex discomfort index
+*/
+/**************************************************************************/
+void humidexDiscomfortIndex(float humidex)
 {
   if ((humidex >= 21 ) && (humidex < 27))
   {
@@ -248,8 +329,15 @@ void HumidexDiscomfortIndex(float humidex)
   }
 }
 
-/* windchill discomfort levels */
-void WindchillDiscomfortIndex(float windchill)
+/**************************************************************************/
+/*
+    windchillDiscomfortIndex()
+
+    Describes levels of windchill discomfort index
+*/
+/**************************************************************************/
+
+void windchillDiscomfortIndex(float windchill)
 {
   if (windchill >= -24)
   {
