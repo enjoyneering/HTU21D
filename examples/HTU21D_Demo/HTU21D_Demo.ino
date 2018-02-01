71dd65d896b0b3495825f0b68d8fe682bcc2d780
enjoyneering <enjoyneering79@gmail.com>
2018-02-01 15:15:32 -0500

    Update HTU21D_Demo.ino

--- examples/HTU21D_Demo/HTU21D_Demo.ino
+++ examples/HTU21D_Demo/HTU21D_Demo.ino
@@ -1,6 +1,7 @@
 /***************************************************************************************************/
 /* 
-  This is an Arduino example for SHT21, HTU21D Digital Humidity & Temperature Sensor
+  This is an Arduino code calculates Dew Point, Humidex and Winchill temperature by using
+  DSHT21, HTU21D or Si70xx Digital Humidity & Temperature Sensor.
 
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
@@ -17,12 +18,44 @@
   NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
   WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
 
+  NOTE:
+  The Humidex is an index number used to describe how hot the weather feels 
+  to the average person, by combining the effect of heat and humidity.
+  Humidex is calculated as:
+                            Humidex = air temperature + h
+                            h = (0.5555) * (e - 10.0)
+                            e = 6.11 * exp(5417.7530 * ((1 / 273.16) - (1 / dewpoint)))
+
+  Wind chill is an index number used to describe how cold the weather feels
+  to the average person, by combining the effect of cold and wind speeds.
+  Wind chill is calculated:
+                            Windchill = 13.12 + 0.6215 * ta - 11.37 * v^0.16 + 0.3965 * ta * v^0.16
+                            ta = air temperature below 10C
+                            v  = wind velocity greater than 4.8 km/h
+
   GNU GPL license, all text above must be included in any redistribution, see link below for details
   - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
-#include <Wire.h>
+#include <Wire.h>              //https://github.com/enjoyneering/ESP8266-I2C-Driver
 #include <HTU21D.h>
+#include <LiquidCrystal_I2C.h> //https://github.com/enjoyneering/LiquidCrystal_I2C
+#include <ESP8266WiFi.h>
+
+#define LCD_ROWS      4        //qnt. of lcd rows
+#define LCD_COLUMNS   20       //qnt. of lcd columns
+#define DEGREE_SYMBOL 0xDF     //degree symbol from the LCD ROM
+#define WIND_SPEED    5        //assuming wind speed is 5 m/sec
+#define ERROR         255      //error code
+
+uint8_t temperature_icon[8] = {0x04, 0x0A, 0x0A, 0x0A, 0x0A, 0x1F, 0x1F, 0x0E};
+uint8_t humidity_icon[8]    = {0x04, 0x0E, 0x0E, 0x1F, 0x1F, 0x1F, 0x0E, 0x00};
+
+float temperature = 0;
+float humidity    = 0;
+float dewpoint    = 0;
+float humidex     = 0;
+float windchill   = 0;
 
 /*
 HTU21D(resolution)
@@ -32,60 +65,225 @@ HTU21D_RES_RH12_TEMP14 - RH: 12Bit, Temperature: 14Bit, by default
 HTU21D_RES_RH8_TEMP12  - RH: 8Bit,  Temperature: 12Bit
 HTU21D_RES_RH10_TEMP13 - RH: 10Bit, Temperature: 13Bit
 HTU21D_RES_RH11_TEMP11 - RH: 11Bit, Temperature: 11Bit
-*/
-HTU21D myHTU21D(HTU21D_RES_RH12_TEMP14);
+*/ 
+HTU21D            myHTU21D(HTU21D_RES_RH12_TEMP14);
+LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 
 void setup()
 {
+  WiFi.enableAP(false);                                         //disable soft AP
+  WiFi.enableSTA(false);                                        //disable station/client
+
   Serial.begin(115200);
-  
-  while (myHTU21D.begin() != true)
+
+  /* LCD connection check */  
+  while (lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS) != true) //20x4 display, LCD_5x8DOTS pixels size, SDA - D2, SCL - D1
   {
-    Serial.println(F("HTU21D, SHT21 sensor is faild or not connected"));
+    Serial.println("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.");
     delay(5000);
   }
-  Serial.println(F("HTU21D, SHT21 sensor is active"));
-}
+  lcd.print("PCF8574 is OK");
+  delay(1000);
+  lcd.clear();
 
+  /* HTU21D connection check */
+  while (myHTU21D.begin() != true)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("HTU21D error");
+    delay(5000);
+  }
+  lcd.print("HTU21D OK   ");
+  delay(2000);
+  lcd.clear();
+
+  /* load custom symbol to CGRAM */
+  lcd.createChar(0, temperature_icon);
+  lcd.createChar(1, humidity_icon);
+
+  /* prints static text */
+  lcd.setCursor(0, 0);                                                  //set 1-st colum & 1-st row, first colum & row started at zero
+  lcd.write((uint8_t)0);                                                //print custom tempereture symbol
+
+  lcd.setCursor(10, 0);
+  lcd.write((uint8_t)1);                                                //print custom humidity symbol
+
+  lcd.setCursor(0, 1);
+  lcd.print("Dewpoint:");
+
+  lcd.setCursor(0, 2);
+  lcd.print("Humidex :");
+
+  lcd.setCursor(0, 3);
+  lcd.print("Windchill:");
+}
 
 void loop()
 {
-  /* DEMO - 1 */
-  Serial.println(F("DEMO 1: 12 Bit Resolution")); 
-  Serial.print(F("Humidity............: ")); Serial.print(myHTU21D.readHumidity()); Serial.println(F(" +-2%"));
-  Serial.print(F("Compensated Humidity: ")); Serial.print(myHTU21D.readCompensatedHumidity()); Serial.println(F(" +-2%"));
+  humidity    = myHTU21D.readCompensatedHumidity();
+  temperature = myHTU21D.readTemperature();
+  dewpoint    = calculateDewPoint(temperature, humidity);
+  humidex     = calculateHumidex(temperature, dewpoint);
+  windchill   = calculateWindchill(temperature, WIND_SPEED);
 
-  Serial.println(F("DEMO 1: 14 Bit Resolution")); 
-  Serial.print(F("Temperature.........: ")); Serial.print(myHTU21D.readTemperature()); Serial.println(F(" +-0.3C")); 
-  /* DEMO - 2 */
-  Serial.println(F("DEMO 2: 11 Bit Resolution"));
-  myHTU21D.setResolution(HTU21D_RES_RH11_TEMP11);
-  Serial.print(F("Humidity............: ")); Serial.print(myHTU21D.readHumidity()); Serial.println(F(" +-2%"));
-  Serial.print(F("Compensated Humidity: ")); Serial.print(myHTU21D.readCompensatedHumidity()); Serial.println(F(" +-2%"));
-
-  Serial.println(F("DEMO 2: 11 Bit Resolution"));
-  Serial.print(F("Temperature.........: ")); Serial.print(myHTU21D.readTemperature()); Serial.println(F(" +-0.3C"));
-  /* DEMO - 3 */
-  Serial.println(F("DEMO 3: Battery Status"));
-  if (myHTU21D.batteryStatus() == true)
+  /* prints dynamic text & data */
+  lcd.setCursor(1, 0);
+  if (temperature == HTU21D_ERROR)
   {
-    Serial.println(F("Battery.............: OK.  Level > 2.25v"));
+    lcd.print("---");
+  }
+  else
+  { 
+    lcd.print(temperature);
+    lcd.write(DEGREE_SYMBOL);
+  }
+
+  lcd.setCursor(11, 0);
+  if (humidity == HTU21D_ERROR)
+  {
+    lcd.print("---");
+  }
+  else
+  { 
+    lcd.print(humidity);
+    lcd.print("% ");
+  }
+
+  lcd.setCursor(9, 1);
+  if (dewpoint == ERROR)
+  {
+    lcd.print("---");
   }
   else
   {
-    Serial.println(F("Battery.............: LOW. Level < 2.25v"));
+    lcd.print(dewpoint);
+    lcd.write(DEGREE_SYMBOL);
   }
-  /* DEMO - 4 */
-  Serial.println(F("DEMO 4:"));
-  Serial.print(F("Firmware version....: ")); Serial.println(myHTU21D.readFirmwareVersion());
-  /* DEMO - 5 */
-  Serial.println(F("DEMO 5:"));
-  Serial.print(F("Sensor's ID.........: ")); Serial.println(myHTU21D.readDeviceID());
-  /* back to lib. default resolution */
-  myHTU21D.softReset();
-  myHTU21D.setResolution(HTU21D_RES_RH12_TEMP14);
-  /* DEMO - END */
-  Serial.print(F("DEMO starts over again in 20 sec."));
+
+  lcd.setCursor(9, 2);
+  if (humidex == ERROR)
+  {
+    lcd.print("---");
+  }
+  else
+  {  
+    lcd.print(humidex);
+    lcd.write(DEGREE_SYMBOL);
+  }
+
+  lcd.setCursor(9, 3);
+  if (windchill == ERROR)
+  {
+    lcd.print("---");
+  }
+  else
+  {
+    lcd.print(windchill);
+    lcd.write(DEGREE_SYMBOL);
+  }
+
+  /* prints serial data */
+  Serial.print("Humidity............: "); Serial.print(humidity);    Serial.println("%");
+  Serial.print("Temperature.........: "); Serial.print(temperature); Serial.println("C");
+  Serial.print("Dew Point...........: "); Serial.print(dewpoint);    Serial.println("C");
+  Serial.print("Humidex/Feels like..: "); Serial.print(humidex);     Serial.print("C, ");  HumidexDiscomfortIndex(humidex);
+  Serial.print("Windchill/Feels like: "); Serial.print(windchill);   Serial.print("C, ");  WindchillDiscomfortIndex(humidex);
+
   delay(20000);
+}
+ 
+/* dew point calculation */
+float calculateDewPoint(float temperature, float humidity)
+{
+  float a = 17.271;
+  float b = 237.7;
+
+  if (temperature != HTU21D_ERROR && humidity != HTU21D_ERROR)
+  {
+    float gamma    = ((a * temperature) / (b + temperature)) + log(humidity / 100);
+    float dewpoint = (b * gamma) / (a - gamma);
+
+    return (dewpoint);
+  }
+  return ERROR;
+}
+
+/* humidex calculation */
+float calculateHumidex(float temperature, float dewpoint)
+{
+  if (temperature > 5 && temperature != HTU21D_ERROR && dewpoint != ERROR)
+  {
+    float e = 5417.7530 * ((1 / 273.16) - (1 / (273.16 + dewpoint)));
+    float humidex = temperature + 0.5555 * ( 6.11 * exp(e) - 10);
+
+    return (humidex);
+  }
+  return ERROR;
+}
+
+/* windchill calculation */
+float calculateWindchill(float temperature, float velocity)
+{
+  if (temperature != HTU21D_ERROR && temperature <= 5 && velocity >= 5)
+  {
+    float windchill = 13.12 + 0.6215 * temperature - 11.37 * pow(velocity, 0.16) + 0.3965 * temperature * pow(velocity, 0.16);
+
+    return (windchill);
+  }
+  return ERROR;
+}
+
+/* humidex discomfort levels */
+void HumidexDiscomfortIndex(float humidex)
+{
+  if ((humidex >= 21 ) && (humidex < 27))
+  {
+    Serial.println("Little discomfort.");
+  }
+  else if ((humidex >= 27) && (humidex < 35))
+  {
+    Serial.println("Noticeable discomfort.");
+  }
+  else if ((humidex >= 35) && (humidex < 40))
+  {
+    Serial.println("Great discomfort.");
+  }
+  else if ((humidex >= 40) && (humidex < 46))
+  {
+    Serial.println("Intense discomfort.");
+  }
+  else if ((humidex >= 46) && (humidex < 54))
+  {
+    Serial.println("Dangerous discomfort.");
+  }
+  else
+  {
+    Serial.println("Heat stroke warning.");
+  }
+}
+
+/* windchill discomfort levels */
+void WindchillDiscomfortIndex(float windchill)
+{
+  if (windchill >= -24)
+  {
+    Serial.println("Low risk of frostbite.");
+  }
+  else if ((windchill < -24) && (windchill >= -39))
+  {
+    Serial.println("High risk of frostbite within 30 minutes of exposure.");
+  }
+  else if ((windchill < -39) && (windchill >= -47))
+  {
+    Serial.println("High risk of frostbite within 5 to 10 minutes of exposure.");
+  }
+  else if ((windchill < -47) && (windchill >= -54))
+  {
+    Serial.println("High risk of frostbite within 2 to 5 minutes of exposure.");
+  }
+  else
+  {
+    Serial.println("High risk of frostbite within 2 minutes of exposure.");
+  }
 }
