af415c74aeb6567a793729b92d2de9c1dcdf8c7b
enjoyneering <enjoyneering79@gmail.com>
2018-02-01 20:23:22 -0500

    Update HTU21D_Heater_Demo.ino

--- examples/HTU21D_Heater_Demo/HTU21D_Heater_Demo.ino
+++ examples/HTU21D_Heater_Demo/HTU21D_Heater_Demo.ino
@@ -5,18 +5,20 @@
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
 
-  This sensor uses I2C bus to communicate. Two pins are required to interface
+  This sensor uses I2C bus to communicate, specials pins are required to interface
 
-  Connect sensor to pins:  SDA     SCL
-  Uno, Mini, Pro:          A4      A5
-  Mega2560, Due:           20      21
-  Leonardo:                2       3
-  ATtiny85:                0(5)    2/A1(7) (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                            & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-xx:          ANY     ANY     (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             ANY     ANY     (D2 & D1 by default)
+  Connect chip to pins:    SDA        SCL
+  Uno, Mini, Pro:          A4         A5
+  Mega2560, Due:           20         21
+  Leonardo:                2          3
+  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
+                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
+  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
+  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
+  WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
 
-BSD license, all text above must be included in any redistribution
+  GNU GPL license, all text above must be included in any redistribution, see link below for details
+  - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
 #include <Wire.h>
@@ -26,11 +28,11 @@ BSD license, all text above must be included in any redistribution
 HTU21D(resolution)
 
 resolution:
-HTU21D_RES_RH12_TEMP14 - RH: 12Bit. Temperature: 14Bit, by default.
-HTU21D_RES_RH8_TEMP12  - RH: 8Bit.  Temperature: 12Bit.
-HTU21D_RES_RH10_TEMP13 - RH: 10Bit. Temperature: 13Bit.
-HTU21D_RES_RH11_TEMP11 - RH: 11Bit. Temperature: 11Bit.
-*/
+HTU21D_RES_RH12_TEMP14 - RH: 12Bit, Temperature: 14Bit, by default
+HTU21D_RES_RH8_TEMP12  - RH: 8Bit,  Temperature: 12Bit
+HTU21D_RES_RH10_TEMP13 - RH: 10Bit, Temperature: 13Bit
+HTU21D_RES_RH11_TEMP11 - RH: 11Bit, Temperature: 11Bit
+*/ 
 HTU21D myHTU21D(HTU21D_RES_RH12_TEMP14);
 
 
@@ -54,6 +56,8 @@ void loop()
 
   Serial.println(F("DEMO 1: 12 Bit Resolution"));
   Serial.print(F("Humidity...: ")); Serial.print(myHTU21D.readHumidity());    Serial.println(F(" +-2%"));
+
+
   /* DEMO - 2 */
   Serial.println(F("DEMO 2: Built-in Heater - ON for 3 sec."));
   myHTU21D.setHeater(HTU21D_ON);
@@ -61,6 +65,8 @@ void loop()
 
   Serial.print(F("Temperature: ")); Serial.print(myHTU21D.readTemperature()); Serial.println(F(" +-0.5C"));
   Serial.print(F("Humidity...: ")); Serial.print(myHTU21D.readHumidity());    Serial.println(F(" +-2%"));
+
+
   /* DEMO - 3 */
   Serial.println(F("DEMO 3: Built-in Heater - OFF for 10 sec."));
   myHTU21D.setHeater(HTU21D_OFF);
@@ -68,6 +74,8 @@ void loop()
 
   Serial.print(F("Temperature: ")); Serial.print(myHTU21D.readTemperature()); Serial.println(F(" -+0.5C"));
   Serial.print(F("Humidity...: ")); Serial.print(myHTU21D.readHumidity());    Serial.println(F(" +-2%"));
+
+
   /* DEMO - END */
   Serial.print(F("DEMO starts over again in 20 sec."));
   delay(20000);
