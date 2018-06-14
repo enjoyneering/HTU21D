e9d629333d9e25c1358f6f5c202a923ee3a05700
enjoyneering <enjoyneering79@gmail.com>
2018-06-13 21:20:46 -0400

    Update HTU21D_ESP8266-01_LCD_i2c.ino

--- examples/HTU21D_ESP8266-01_LCD_i2c/HTU21D_ESP8266-01_LCD_i2c.ino
+++ examples/HTU21D_ESP8266-01_LCD_i2c/HTU21D_ESP8266-01_LCD_i2c.ino
@@ -1,22 +1,29 @@
 /***************************************************************************************************/
 /* 
   This is an Arduino example for SHT21, HTU21D Digital Humidity & Temperature Sensor shows how to
-  reassign default SDA/SCL pins for ESP8266
+  reassign default SDA/SCL pins for ESP8266-01
 
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
 
-  This sensor uses I2C bus to communicate, specials pins are required to interface
+  This sketch uses I2C bus to communicate, specials pins are required to interface
+  Board:                                    SDA                    SCL
+  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
+  Mega2560, Due............................ 20                     21
+  Leonardo, Micro, ATmega32U4.............. 2                      3
+  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
+  Blue Pill, STM32F103xxxx boards.......... B7*                    B6*
+  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
 
-  Connect chip to pins:    SDA        SCL
-  Uno, Mini, Pro:          A4         A5
-  Mega2560, Due:           20         21
-  Leonardo:                2          3
-  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
-  WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
+                                            *STM32F103xxxx pins B7/B7 are 5v tolerant, but bi-directional
+                                             logic level converter is recommended
+
+  Frameworks & Libraries:
+  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ESP8266 Core          - https://github.com/esp8266/Arduino
+  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
   GNU GPL license, all text above must be included in any redistribution, see link below for details
   - https://www.gnu.org/licenses/licenses.html
@@ -47,7 +54,6 @@ float   humidity    = 0;
 
 /*
 HTU21D(resolution)
-
 resolution:
 HTU21D_RES_RH12_TEMP14 - RH: 12Bit, Temperature: 14Bit, by default
 HTU21D_RES_RH8_TEMP12  - RH: 8Bit,  Temperature: 12Bit
@@ -71,8 +77,10 @@ void setup()
     Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //(F()) saves string to flash & keeps dynamic memory free
     delay(5000);
   }
+
   lcd.print(F("PCF8574 is OK"));
   delay(1000);
+
   lcd.clear();
 
   /* HTU21D connection check */
@@ -82,10 +90,12 @@ void setup()
     lcd.print(F("HTU21D error"));
     delay(5000);
   }
+
   lcd.clear();
 
   lcd.print(F("HTU21D OK"));
   delay(1000);
+
   lcd.clear();
 
   /* load custom symbol to CGRAM */
@@ -93,7 +103,7 @@ void setup()
   lcd.createChar(1, humidity_icon,    'F');
 
   /* prints static text */
-  lcd.setCursor(0, 0);                                          //set 1-st colum & 1-st row, first colum & row started at zero
+  lcd.setCursor(0, 0);                                          //set 1-st colum & 1-st row
   lcd.write(0);                                                 //print custom tempereture symbol
 
   lcd.setCursor(0, 1);                              
@@ -107,17 +117,21 @@ void loop()
 
   /* prints dynamic temperature data */
   lcd.setCursor(1, 0);
+
   if   (temperature != HTU21D_ERROR) lcd.print(temperature);
   else                               lcd.print(F("xxx"));
+
   lcd.write(DEGREE_SYMBOL);
   lcd.write(SPACE_SYMBOL);
 
   /* prints dynamic humidity data */
   lcd.setCursor(1, 1);
+
   if   (humidity != HTU21D_ERROR) lcd.print(humidity);
   else                            lcd.print(F("xxx"));
+
   lcd.print(F("%"));
   lcd.write(SPACE_SYMBOL);
 
-  delay(100000);
+  delay(10000);
 }
