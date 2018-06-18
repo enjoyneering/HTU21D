0b9e4cd8a071d7927940a2282e15bcd72abf4527
enjoyneering <enjoyneering79@gmail.com>
2018-06-18 19:47:36 -0400

    Update HTU21D_Demo.ino

--- examples/HTU21D_Demo/HTU21D_Demo.ino
+++ examples/HTU21D_Demo/HTU21D_Demo.ino
@@ -6,18 +6,25 @@
   sourse code: https://github.com/enjoyneering/
 
   This sensor uses I2C bus to communicate, specials pins are required to interface
+  Board:                                    SDA                    SCL
+  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
+  Mega2560, Due............................ 20                     21
+  Leonardo, Micro, ATmega32U4.............. 2                      3
+  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
+  Blue Pill, STM32F103xxxx boards.......... PB7*                   PB6*
+  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
 
-  Connect chip to pins:    SDA        SCL
-  Uno, Mini, Pro:          A4         A5
-  Mega2560, Due:           20         21
-  Leonardo:                2          3
-  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
-  WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
+                                           *STM32F103xxxx pins B7/B7 are 5v tolerant, but bi-directional
+                                            logic level converter is recommended
 
-  GNU GPL license, all text above must be included in any redistribution, see link below for details
+  Frameworks & Libraries:
+  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ESP8266 Core          - https://github.com/esp8266/Arduino
+  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
+
+  GNU GPL license, all text above must be included in any redistribution, see link below for details:
   - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
@@ -39,10 +46,11 @@ HTU21D myHTU21D(HTU21D_RES_RH12_TEMP14);
 void setup()
 {
   Serial.begin(115200);
+  Serial.println();
   
   while (myHTU21D.begin() != true)
   {
-    Serial.println(F("HTU21D, SHT21 sensor is faild or not connected"));
+    Serial.println(F("HTU21D, SHT21 sensor is faild or not connected")); //(F()) saves string to flash & keeps dynamic memory free
     delay(5000);
   }
   Serial.println(F("HTU21D, SHT21 sensor is active"));
@@ -52,21 +60,21 @@ void setup()
 void loop()
 {
   /* DEMO - 1 */
-  Serial.println(F("DEMO 1: 12 Bit Resolution")); 
-  Serial.print(F("Humidity............: ")); Serial.print(myHTU21D.readHumidity()); Serial.println(F(" +-2%"));
+  Serial.println(F("DEMO 1: 12-Bit Resolution"));
+  Serial.print(F("Humidity............: ")); Serial.print(myHTU21D.readHumidity());            Serial.println(F(" +-2%"));
   Serial.print(F("Compensated Humidity: ")); Serial.print(myHTU21D.readCompensatedHumidity()); Serial.println(F(" +-2%"));
 
-  Serial.println(F("DEMO 1: 14 Bit Resolution")); 
+  Serial.println(F("DEMO 1: 14-Bit Resolution")); 
   Serial.print(F("Temperature.........: ")); Serial.print(myHTU21D.readTemperature()); Serial.println(F(" +-0.3C"));
 
  
   /* DEMO - 2 */
-  Serial.println(F("DEMO 2: 11 Bit Resolution"));
+  Serial.println(F("DEMO 2: 11-Bit Resolution"));
   myHTU21D.setResolution(HTU21D_RES_RH11_TEMP11);
-  Serial.print(F("Humidity............: ")); Serial.print(myHTU21D.readHumidity()); Serial.println(F(" +-2%"));
+  Serial.print(F("Humidity............: ")); Serial.print(myHTU21D.readHumidity());            Serial.println(F(" +-2%"));
   Serial.print(F("Compensated Humidity: ")); Serial.print(myHTU21D.readCompensatedHumidity()); Serial.println(F(" +-2%"));
 
-  Serial.println(F("DEMO 2: 11 Bit Resolution"));
+  Serial.println(F("DEMO 2: 11-Bit Resolution"));
   Serial.print(F("Temperature.........: ")); Serial.print(myHTU21D.readTemperature()); Serial.println(F(" +-0.3C"));
 
 
