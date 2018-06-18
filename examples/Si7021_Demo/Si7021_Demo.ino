3cdecddd18d28562d42f66c65e78f0b66ef1bc29
enjoyneering <enjoyneering79@gmail.com>
2018-06-18 19:50:22 -0400

    Update Si7021_Demo.ino

--- examples/Si7021_Demo/Si7021_Demo.ino
+++ examples/Si7021_Demo/Si7021_Demo.ino
@@ -1,21 +1,29 @@
 /***************************************************************************************************/
 /* 
-  This is an example for Si7021 Digital Humidity & Temperature Sensor
+  This is an Arduino example for SHT21, HTU21D Digital Humidity & Temperature Sensor shows how to
+  reassign default SDA/SCL pins for ESP8266-01
 
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
 
-  This sensor uses I2C bus to communicate, specials pins are required to interface
+  This sketch uses I2C bus to communicate, specials pins are required to interface
+  Board:                                    SDA                    SCL
+  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
+  Mega2560, Due............................ 20                     21
+  Leonardo, Micro, ATmega32U4.............. 2                      3
+  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
+  Blue Pill, STM32F103xxxx boards.......... PB7*                   PB6*
+  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
 
-  Connect chip to pins:    SDA        SCL
-  Uno, Mini, Pro:          A4         A5
-  Mega2560, Due:           20         21
-  Leonardo:                2          3
-  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
-  WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
+                                           *STM32F103xxxx pins B7/B7 are 5v tolerant, but bi-directional
+                                            logic level converter is recommended
+
+  Frameworks & Libraries:
+  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ESP8266 Core          - https://github.com/esp8266/Arduino
+  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
   GNU GPL license, all text above must be included in any redistribution, see link below for details
   - https://www.gnu.org/licenses/licenses.html
@@ -42,29 +50,29 @@ void setup()
   
   while (myHTU21D.begin() != true)
   {
-    Serial.println("Si7021 sensor is faild or not connected");
+    Serial.println(F("Si7021 sensor is faild or not connected"));
     delay(5000);
   }
-  Serial.println("Si7021 sensor is active");
+  Serial.println(F("Si7021 sensor is active"));
 }
 
 void loop()
 {
   /* DEMO - 1 */
-  Serial.println(F("DEMO 1: 12 Bit Resolution")); 
+  Serial.println(F("DEMO 1: 12-Bit Resolution")); 
   Serial.print(F("Compensated Humidity: ")); Serial.print(myHTU21D.readHumidity()); Serial.println(F(" +-2%"));
 
-  Serial.println(F("DEMO 1: 14 Bit Resolution")); 
+  Serial.println(F("DEMO 1: 14-Bit Resolution")); 
   Serial.print(F("Temperature.........: ")); Serial.print(myHTU21D.readTemperature(SI70xx_TEMP_READ_AFTER_RH_MEASURMENT)); Serial.println(F(" +-0.5C"));
 
 
   /* DEMO - 2 */
-  Serial.println(F("DEMO 2: 11 Bit Resolution"));
+  Serial.println(F("DEMO 2: 11-Bit Resolution"));
   myHTU21D.setResolution(HTU21D_RES_RH11_TEMP11);
 
   Serial.print(F("Compensated Humidity: ")); Serial.print(myHTU21D.readHumidity()); Serial.println(F(" +-2%"));
 
-  Serial.println(F("DEMO 2: 11 Bit Resolution"));
+  Serial.println(F("DEMO 2: 11-Bit Resolution"));
   Serial.print(F("Temperature.........: ")); Serial.print(myHTU21D.readTemperature(SI70xx_TEMP_READ_AFTER_RH_MEASURMENT)); Serial.println(F(" +-0.5C"));
 
 
