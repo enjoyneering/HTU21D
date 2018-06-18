b60e3235cb8d69cdba9337397afb982ac44e9e04
enjoyneering <enjoyneering79@gmail.com>
2018-06-17 22:13:16 -0400

    Update HTU21D_ESP8266_Humidex_Windchill.ino

--- examples/HTU21D_ESP8266_Humidex_Windchill/HTU21D_ESP8266_Humidex_Windchill.ino
+++ examples/HTU21D_ESP8266_Humidex_Windchill/HTU21D_ESP8266_Humidex_Windchill.ino
@@ -1,22 +1,29 @@
 /***************************************************************************************************/
 /* 
-  This is an Arduino code calculates Dew Point, Humidex and Winchill temperature by using
-  DSHT21, HTU21D or Si70xx Digital Humidity & Temperature Sensor.
+  This is an Arduino example for SHT21, HTU21D Digital Humidity & Temperature Sensor shows how to
+  reassign default SDA/SCL pins for ESP8266-01
 
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
 
-  This sensor uses I2C bus to communicate, specials pins are required to interface
+  This sketch uses I2C bus to communicate, specials pins are required to interface
+  Board:                                    SDA                    SCL
+  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
+  Mega2560, Due............................ 20                     21
+  Leonardo, Micro, ATmega32U4.............. 2                      3
+  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
+  Blue Pill, STM32F103xxxx boards.......... PB7*                   PB6*
+  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
 
-  Connect chip to pins:    SDA        SCL
-  Uno, Mini, Pro:          A4         A5
-  Mega2560, Due:           20         21
-  Leonardo:                2          3
-  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
-  WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
+                                            *STM32F103xxxx pins B7/B7 are 5v tolerant, but bi-directional
+                                             logic level converter is recommended
+
+  Frameworks & Libraries:
+  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ESP8266 Core          - https://github.com/esp8266/Arduino
+  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
   GNU GPL license, all text above must be included in any redistribution, see link below for details
   - https://www.gnu.org/licenses/licenses.html
@@ -36,8 +43,8 @@
 #define WIND_SPEED    5        //assuming wind speed is 5 m/sec
 #define ERROR         255      //error code
 
-uint8_t temperature_icon[8] = {0x04, 0x0A, 0x0A, 0x0A, 0x0A, 0x1F, 0x1F, 0x0E};
-uint8_t humidity_icon[8]    = {0x04, 0x0E, 0x0E, 0x1F, 0x1F, 0x1F, 0x0E, 0x00};
+const uint8_t temperature_icon[8] PROGMEM = {0x04, 0x0A, 0x0A, 0x0A, 0x0A, 0x1F, 0x1F, 0x0E}; //PROGMEM saves variable to flash & keeps dynamic memory free
+const uint8_t humidity_icon[8]    PROGMEM = {0x04, 0x0E, 0x0E, 0x1F, 0x1F, 0x1F, 0x0E, 0x00};
 
 float temperature = 0;
 float humidity    = 0;
@@ -67,32 +74,36 @@ LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POS
 /**************************************************************************/
 void setup()
 {
-  WiFi.enableAP(false);                                         //disable soft AP
-  WiFi.enableSTA(false);                                        //disable station/client
+  WiFi.persistent(false);                                       //disable saving wifi config into SDK flash area
+  WiFi.forceSleepBegin();                                       //disable swAP & station by calling "WiFi.mode(WIFI_OFF)" & put modem to sleep
 
   Serial.begin(115200);
 
   /* LCD connection check */  
   while (lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS) != true) //20x4 display, LCD_5x8DOTS pixels size, SDA - D2, SCL - D1
   {
-    Serial.println("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.");
+    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //(F()) saves string to flash & keeps dynamic memory
     delay(5000);
   }
-  lcd.print("PCF8574 is OK");
+
+  lcd.print(F("PCF8574 is OK"));
   delay(1000);
+
   lcd.clear();
 
   /* HTU21D connection check */
   while (myHTU21D.begin() != true)
   {
     lcd.setCursor(0, 0);
-    lcd.print("HTU21D error");
+    lcd.print(F("HTU21D error"));
     delay(5000);
   }
+
   lcd.clear();
 
-  lcd.print("HTU21D OK");
+  lcd.print(F("HTU21D OK"));
   delay(1000);
+
   lcd.clear();
 
   /* load custom symbol to CGRAM */
@@ -101,19 +112,19 @@ void setup()
 
   /* prints static text */
   lcd.setCursor(0, 0);                                                  //set 1-st colum & 1-st row, first colum & row started at zero
-  lcd.write((uint8_t)0);                                                //print custom tempereture symbol
+  lcd.write(0);                                                         //print custom tempereture symbol
 
   lcd.setCursor(10, 0);
-  lcd.write((uint8_t)1);                                                //print custom humidity symbol
+  lcd.write(1);                                                         //print custom humidity symbol
 
   lcd.setCursor(0, 1);
-  lcd.print("Dewpoint :");
+  lcd.print(F("Dewpoint :"));
 
   lcd.setCursor(0, 2);
-  lcd.print("Humidex  :");
+  lcd.print(F("Humidex  :"));
 
   lcd.setCursor(0, 3);
-  lcd.print("Windchill:");
+  lcd.print(F("Windchill:"));
 }
 
 /**************************************************************************/
@@ -135,44 +146,44 @@ void loop()
   /* prints dynamic temperature data */
   lcd.setCursor(1, 0);
   if   (temperature != HTU21D_ERROR) lcd.print(temperature);
-  else                               lcd.print("xxx");
+  else                               lcd.print(F("xxx"));
   lcd.write(DEGREE_SYMBOL);
   lcd.write(SPACE_SYMBOL);
 
   /* prints dynamic humidity data */
   lcd.setCursor(11, 0);
   if   (humidity != HTU21D_ERROR) lcd.print(humidity);
-  else                            lcd.print("xxx");
+  else                            lcd.print(F("xxx"));
   lcd.print("%");
   lcd.write(SPACE_SYMBOL);
 
   /* prints dynamic dewpoint data */
   lcd.setCursor(10, 1);
   if   (dewpoint != ERROR) lcd.print(dewpoint);
-  else                     lcd.print("xxx");
+  else                     lcd.print(F("xxx"));
   lcd.write(DEGREE_SYMBOL);
   lcd.write(SPACE_SYMBOL);
 
   /* prints dynamic humidex data */
   lcd.setCursor(10, 2);
   if   (humidex != ERROR) lcd.print(humidex);
-  else                    lcd.print("xxx");
+  else                    lcd.print(F("xxx"));
   lcd.write(DEGREE_SYMBOL);
   lcd.write(SPACE_SYMBOL);
 
   /* prints dynamic windchill data */
   lcd.setCursor(10, 3);
   if   (windchill != ERROR) lcd.print(windchill);
-  else                      lcd.print("xxx");
+  else                      lcd.print(F("xxx"));
   lcd.write(DEGREE_SYMBOL);
   lcd.write(SPACE_SYMBOL);
 
   /* prints serial data */
-  Serial.print("Humidity............: "); Serial.print(humidity);    Serial.println("%");
-  Serial.print("Temperature.........: "); Serial.print(temperature); Serial.println("C");
-  Serial.print("Dew Point...........: "); Serial.print(dewpoint);    Serial.println("C");
-  Serial.print("Humidex/Feels like..: "); Serial.print(humidex);     Serial.print("C, "); humidexDiscomfortIndex(humidex);
-  Serial.print("Windchill/Feels like: "); Serial.print(windchill);   Serial.print("C, "); windchillDiscomfortIndex(humidex);
+  Serial.print(F("Humidity............: ")); Serial.print(humidity);    Serial.println(F("%"));
+  Serial.print(F("Temperature.........: ")); Serial.print(temperature); Serial.println(F("C"));
+  Serial.print(F("Dew Point...........: ")); Serial.print(dewpoint);    Serial.println(F("C"));
+  Serial.print(F("Humidex/Feels like..: ")); Serial.print(humidex);     Serial.print(F("C, ")); humidexDiscomfortIndex(humidex);
+  Serial.print(F("Windchill/Feels like: ")); Serial.print(windchill);   Serial.print(F("C, ")); windchillDiscomfortIndex(humidex);
 
   delay(20000);
 }
